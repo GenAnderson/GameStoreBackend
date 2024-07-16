74ea79687179b718159a8db214d702e4332c5c76
GenAnderson <genchung1@outlook.com>
2024-07-16 12:19:38 -1000

    published with db file

--- obj/Release/net8.0/GameStore.Api.AssemblyInfo.cs
+++ obj/Release/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6106ae9bcfaf9c4bb165a9da0f2532450e8634f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2866ba9931de57915b529b124aaa96914a87ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
